feceeb6aecee8b6da4f474d23eb7beb0fc6ff63c
Fuatz <fuatcansarimsakci@gmail.com>
2023-04-07 20:14:02 +0300

    TheOverTimePrices

---
+++ src/ExtraMesai.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ExtraMesai {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double Maas,AylikCalismaSaati,MesaiSaati,MesaiUcreti;
+
+        System.out.println("Mesai ücretinizi hesaplamak için gerekli değerleri giriniz : ");
+        System.out.println("Maaşınız = ");
+        Maas = input.nextDouble();
+        System.out.println("Aylık Çalışma saatinizi giriniz : ");
+        AylikCalismaSaati = input.nextDouble();
+        System.out.println("Bu ay ki mesai saatinizi giriniz : ");
+        MesaiSaati = input.nextDouble();
+        MesaiUcreti = ((Maas / AylikCalismaSaati)* 1.5)*MesaiSaati;
+        System.out.println("Mesai ücretiniz : " +MesaiUcreti );
+    }
+}
