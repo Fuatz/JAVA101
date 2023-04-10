776e8b8e79a1c43b50ee0adc8416524e30445418
Fuatz <fuatcansarimsakci@gmail.com>
2023-04-11 01:15:02 +0300

    etkinlik

--- src/Pratices/HavaSicakligineGoreEtkinlik.java
+++ src/Pratices/HavaSicakligineGoreEtkinlik.java
@@ -9,8 +9,8 @@ public class HavaSicakligineGoreEtkinlik {
         heat = input.nextInt();
 
         if (heat < 5)                      { System.out.println("Kayağa gidiniz.");
-        } else if (heat > 5  && heat < 10)  { System.out.println("Sinemaya git");
-        } else if (heat > 10 && heat < 15)  { System.out.println("Piknik yada Sinemaya git.");
+        } else if (heat > 5  && heat < 10) { System.out.println("Sinemaya git");
+        } else if (heat > 10 && heat < 15) { System.out.println("Piknik yada Sinemaya git.");
         } else if (heat > 15 && heat < 25) { System.out.println("Pikniğe git.");
         } else                             { System.out.println("Yüzmeye git.");}
 
