4c68ebac3f8526da2399c0698f05875cdb50b584
Fuatz <fuatcansarimsakci@gmail.com>
2023-04-11 00:15:15 +0300

    Etkinlik

---
+++ src/Pratices/HavaSicakligineGoreEtkinlik.java
@@ -0,0 +1,19 @@
+package Pratices;
+import java.util.Scanner;
+public class HavaSicakligineGoreEtkinlik {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int heat;
+
+        System.out.println("Etkinlik için sıcaklık giriniz :");
+        heat = input.nextInt();
+
+        if (heat < 5)                      { System.out.println("Kayağa gidiniz.");
+        } else if (heat >= 5 && heat < 10)  { System.out.println("Sinemaya git");
+        } else if (heat >= 10 && heat <=15)  { System.out.println("Piknik yada Sinemaya git.");
+        } else if (heat > 15 && heat <= 25) { System.out.println("Pikniğe git.");
+        } else                             { System.out.println("Yüzmeye git.");}
+
+        }
+    }
+
