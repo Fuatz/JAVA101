8a420e161eb2f2ae528929eab3fd532a2a83c503
Fuatz <fuatcansarimsakci@gmail.com>
2023-04-17 20:16:35 +0300

    Yakit

--- src/Pratices/YolYakitMasrafi.java
+++ src/Pratices/YolYakitMasrafi.java
@@ -1,17 +1,21 @@
 package Pratices;
-
 import java.util.Scanner;
-
-public class YolMasrafi {
+public class YolYakitMasrafi {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         double km, yakitFiyati, yakitSarfiyati, totalYakitFiyati;
-        yakitFiyati = 22;
-        yakitSarfiyati = 8;
+
+        System.out.println("Benzinin litre fiyatını giriniz : ");
+        yakitFiyati = input.nextDouble();
+
+        System.out.println("Yakıt Sarfiyatınızı giriniz :");
+        yakitSarfiyati = input.nextDouble();
 
         System.out.println("Gideceğinz yolun km giriniz : ");
         km = input.nextDouble();
-        totalYakitFiyati = km / 100 * yakitSarfiyati * yakitFiyati;
+
+        totalYakitFiyati = km * yakitFiyati * yakitSarfiyati / 100;
 
         System.out.println("Total yakıt masrafınız : " + totalYakitFiyati);
 
