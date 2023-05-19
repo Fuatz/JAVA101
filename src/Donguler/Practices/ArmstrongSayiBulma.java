2f8ee877cac45c64ce598e1094e2d85c17b2a4bb
Fuatz <fuatcansarimsakci@gmail.com>
2023-05-19 20:57:27 +0300

    2

---
+++ src/Donguler/Practices/ArmstrongSayiBulma.java
@@ -0,0 +1,9 @@
+package Donguler.Practices;
+import java.util.Scanner;
+public class ArmstrongSayiBulma {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        
+    }
+}
